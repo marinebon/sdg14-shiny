6ac3be48a1c9d5f05fd4573d91cb0860b6efaa23
Ben Best <ben@ecoquants.com>
2017-08-09 17:16:54 +0000

    tab pages

--- v2/ui.R
+++ v2/ui.R
@@ -6,17 +6,27 @@ dashboardPage(
     uiOutput('ui_lyr'),               
     selectizeInput('sel_eez', 'EEZ - Territory', c('', eez_sf$sov_ter), selected='')),
   dashboardBody(
-    box(
-      title='Plot',
-      conditionalPanel(
-        condition = "output.grd_type=='chl'",
-        dygraphOutput('ts_dygraph'),
-        collapsible=T),
-      conditionalPanel(
-        condition = "output.grd_type=='seascape'",
-        streamgraphOutput('ts_streamgraph')),
-      collapsible=T),
-    box(
-      title='Map',
-      leafletOutput('map', height = 550),
-      collapsible=T)))
+    tabItems(
+      # First tab content
+      tabItem(
+        tabName ='Global',
+        fluidRow(
+          box(
+            title='Plot',
+            conditionalPanel(
+              condition = "output.grd_type=='chl'",
+              dygraphOutput('ts_dygraph')),
+            collapsible=T),
+          conditionalPanel(
+            condition = "output.grd_type=='seascape'",
+            streamgraphOutput('ts_streamgraph')),
+          collapsible=T),
+        box(
+          title='Map',
+          leafletOutput('map', height = 550),
+          collapsible=T)),
+      tabItem(
+        tabName ='Local',
+        fluidRow(
+          box(
+            title='Infographic')))) ))
