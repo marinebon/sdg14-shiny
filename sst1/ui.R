03628c3cd4da4d1e14668e1031b18b5a24784546
Ben Best <ben@ecoquants.com>
2017-10-16 10:39:08 -0700

    sst1 shinydashboard minimal map w/ date slider

--- sst1/ui.R
+++ sst1/ui.R
@@ -1,20 +1,31 @@
-shinyUI(fluidPage(
-  
-  titlePanel("Environmental mapper prototype using WMS on GeoServer"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(
-        'sel_env', 'Variable:',
-        c('Seascape','SST','Chl a')),
+dashboardPage(
+  title = 'SDG14: env WMS prototype',
+  dashboardHeader(
+    title=tagList(icon('tint'), 'SDG14: Life in the Sea'),
+    titleWidth = 250),
+  dashboardSidebar(
+    width = 250,
+    sidebarMenu(
+      #id = 'menu_var',
+      menuItem(
+        text='Environmental', icon=icon('thermometer'), tabName='env', selected=T, startExpanded=T,
+        menuSubItem(
+          'Seascape', tabName='seascape', icon=icon("angle-double-right"), selected=T),
+        menuSubItem(
+          'Temperature', tabName='sst', icon=icon("angle-double-right")),
+        menuSubItem(
+          'Chlorophyll', tabName='chl', icon=icon("angle-double-right"))),
       selectInput(
         'sel_temporal', 'Temporal:', 
-        c('Climatological', 'Contemporaneous'))),
-
-    mainPanel(
-      leafletOutput('map', height = 550),
-      sliderInput(
-        'sel_ym', 'Date', 
-        min = dates[length(dates)], max = dates[1], value = dates[1],
-        step = 30, animate = T, timeFormat='%b %Y', width='100%')))
-))
+        c('Contemporaneous','Climatological')))),
+  dashboardBody(
+    fluidRow(
+      tabBox(
+        id = 'tabset_viz', width=12,
+        tabPanel(
+          'Spatial',
+          leafletOutput('map', height = 550),
+          sliderInput(
+            'sel_ym', 'Date:', 
+            min = dates[length(dates)], max = dates[1], value = dates[1],
+            step = 30, animate = T, timeFormat='%b %Y', width='100%'))))))
